75272c2fde88d6886baa07e07c9380ca7799b4c8
Sam Seung Min Shin <shinseungmin9498@gmail.com>
2026-04-05 23:06:49 -0400

    changed origins

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["https://jbt-cyan.vercel.app/"],
+    allow_origins = ["https://jbt-cyan.vercel.app"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
